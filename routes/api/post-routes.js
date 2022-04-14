31a57d028440904b57a59ac038de5e7071094c9e
Your Name <kalaitzidispaul@gmail.com>
2022-04-14 15:18:17 -0400

    added delete and put routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,6 +5,7 @@ const { Post, User }= require('../../models')
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: ['id', 'post_url', 'title', 'created_at'],
+      order: [['created_at', 'DESC']],
       include: [
         {
           model: User,
@@ -59,4 +60,47 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+
+router.put('./:id', (req,res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({ message: 'No Post found with this id'})
+            return
+        }
+        res.json(dbPostData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.satus(500).json(err)
+       })
+})
+router.delete('/:id', (req,res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({message: 'No Post found with this id'})
+            return
+        }
+        res.json(dbPostData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router

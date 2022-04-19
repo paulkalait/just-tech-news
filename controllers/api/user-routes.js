c132f8095f44b961e51029f5e9e8dcdfe4358cb5
Your Name <kalaitzidispaul@gmail.com>
2022-04-19 13:41:05 -0400

    added log in and log out routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,11 +60,17 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData)
+      })
+
+    })
+    
 });
 
 router.post('/login', (req, res) => {
@@ -86,9 +92,28 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      //decalre session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true
+
+      res.json({ user: dbUserData, message: "You are now logged in"})
+    });
   });
 });
+
+//route to log out
+router.post('/logout', (req, res) => {
+  if(req.session.loggedIn){
+    req.session.destroy (() => {
+      res.status(204).end()
+    })
+  }
+  else{
+    res.status(404).end()
+  }
+})
 
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
